3a13aeef6859cf7c3cda5a5570de56fb30537c3d
robertbl22 <realmadvertising@Roberts-Mac-Pro.local>
2014-02-28 17:34:04 -0500

    Added Corridor Picker map with hot spots.

--- app/scripts/directives/corridor-picker/corridorPicker-dir.js
+++ app/scripts/directives/corridor-picker/corridorPicker-dir.js
@@ -1,12 +1,52 @@
 'use strict';
 
 angular.module('gccApp')
-.directive('corridorPicker', function () {
+.directive('corridorPicker', function ($stateParams) {
 	return {
 		templateUrl: 'scripts/directives/corridor-picker/corridorPicker.html',
 		restrict: 'A',
 		controller: 'CorridorPickerCtrl',
 		link: function(scope, element, attrs) {
+
+			var img = element.find('#GeorgiaMap');
+
+			var mapsterOptions = {
+				singleSelect : true,
+				fillColor : 'FF7300',
+				stroke : true,
+				strokeColor : 'FFFFFF',
+				strokeWidth : 1,
+				clickNavigate : true,
+				showToolTip : true
+			};
+
+			img.mapster(mapsterOptions);
+
+			scope.$on('$stateChangeSuccess', function () {
+				if($stateParams.corridorId) {
+					var areaId = '#' + $stateParams.corridorId;
+					$(areaId).mapster('select');
+				}
+			});
+
+			/*
+			NOTE: We use a throttled 'resize' event override 
+			because IE will fire events a crazy number 
+			of times until the browser crashes.
+			jquery.unevent.js
+			*/
+			$(window).on('resize', resizeImageMap, 50);
+
+			function resizeImageMap() {
+				img.mapster('resize', element.width(), '', 0);
+			};
+
+			function getMapWidth() {
+				var colWidth = element.width();
+				var newWidth = Math.floor(colWidth * 0.9);
+				return newWidth;
+			};
+
 		}
 	};
 });
