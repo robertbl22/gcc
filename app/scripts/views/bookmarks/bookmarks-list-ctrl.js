3e6728449de27b5e7fa5252eb78e40f7c4d2355a
robertbl22 <realmadvertising@Roberts-Mac-Pro.local>
2014-03-17 14:57:11 -0400

    Fixed bookmarks modal to UI-Utils

--- app/scripts/views/bookmarks/bookmarks-list-ctrl.js
+++ app/scripts/views/bookmarks/bookmarks-list-ctrl.js
@@ -1,19 +1,32 @@
 'use strict';
 
 angular.module('gccApp')
-.controller('BookmarksListCtrl', function ($scope, $rootScope, BookmarksSvc) {
+.controller('BookmarksListCtrl', function ($scope, $rootScope, $modal, BookmarksSvc) {
 
 	BookmarksSvc.load();
 	$scope.Bookmarks = BookmarksSvc.collection;
 	$scope.BookmarksIndex = BookmarksSvc.index;
 	$rootScope.scrollTop();
 
-	$scope.deleteAllBookmarks = function() {
-		BookmarksSvc.deleteAll();
+	$scope.showConfirmDeleteAllModal = function() {
+		$modal.open({
+			templateUrl: 'confirmDeleteAllModal.html',
+			controller: ModalInstanceCtrl
+		});
 	};
 
 	$scope.deleteBookmark = function(property) {
 		BookmarksSvc.deleteSingle(property.OBJECTID);
 	};
+
+	var ModalInstanceCtrl = ['$scope', '$modalInstance', function($scope, $modalInstance) {
+		$scope.deleteAllBookmarks = function() {
+			BookmarksSvc.deleteAll();
+			$modalInstance.close();
+		};
+		$scope.close = function() {
+			$modalInstance.dismiss('cancel');
+		}
+	}];
 	
 });
